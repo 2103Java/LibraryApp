0065851ba871ccbb090251c2a19fe0049396bd83
Ben30000 <47725693+Ben30000@users.noreply.github.com>
2021-04-02 19:10:37 -0500

    Added serialization for reading/writing Book data

--- src/main/java/com/revature/MainDriver.java
+++ src/main/java/com/revature/MainDriver.java
@@ -1,5 +1,17 @@
 package com.revature;
 
+import java.awt.List;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -21,6 +33,53 @@ public class MainDriver {
 	
 	public static void inialiseValues() {
 		
+		String filename = "./Books.txt";
+		
+
+		Object obj = null;
+		
+		ObjectInputStream ois = null;
+		try {
+			
+			ois = new ObjectInputStream(new FileInputStream(filename));
+			
+			while ( (obj = ois.readObject()) != null) {
+				Book curBook = (Book)obj;
+				System.out.println((curBook));
+			}
+
+			
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		 catch(EOFException e) {
+				//System.out.println("EOF");
+			}
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		finally {
+		
+			try {
+				ois.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		
+		
+		
+		
+		
+		/*
 		Book f1 = new Book(0,"Dune","FAkeAuthor1",true);
 		Book f2 = new Book(0,"Expanse1","FAkeAuthor2",false);
 		Book f3 = new Book(0,"Harry Potter ","FAkeAuthor3",false);
@@ -55,20 +114,59 @@ public class MainDriver {
 		userSet = new HashSet<>();
 		userSet.add(u);
 		userSet.add(u2);
+		*/
 		
+		
+	}
+	
+	public static void writeBooks(ArrayList<Book> books) {
+		
+		
+		String filename = "./Books.txt";
+
+
+		try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filename))){
+			
+			for (Book bk : books)
+			oos.writeObject(bk);
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		
 	}
 	
 	public static void main(String[] args) {
 		
+		System.out.println("main() start");
+		
+		Book book1 = new Book(1, "Welcome to London", "J.R. Briggleton", true);
+		Book book2 = new Book(2, "The Great Book", "Arthur", true);
+		Book book3 = new Book(3, "Good Book", "Patrickson", true);
+		Book book4 = new Book(4, "The Mysterious Forest", "Frank", true);
+		Book book5 = new Book(5, "The Mysterious Forest 2", "Frank", true);
+		ArrayList<Book> books = new ArrayList<Book>();
+		books.add(book1);
+		books.add(book2);
+		books.add(book3);
+		books.add(book4);
+		books.add(book5);
+		
+		writeBooks(books);
+		inialiseValues();
+		/*
 		UserRepo ur = new BudgetDatabaseUsers(userSet); //Repo layer
 		BookRepo br = new StoreRoom(libraryBooks);
 		
 		ServiceLayer office = new Office(ur,br); //service layer 
 		
 		FrontOffice fo = new FrontOffice(office); //presentation layer
-		
+		*/
 		
 		
 	}
--- src/main/java/com/revature/models/Book.java
+++ src/main/java/com/revature/models/Book.java
@@ -1,6 +1,8 @@
 package com.revature.models;
 
-public class Book {
+import java.io.Serializable;
+
+public class Book implements Serializable {
 	
 	//getters and setters
 	//constructor(s)
